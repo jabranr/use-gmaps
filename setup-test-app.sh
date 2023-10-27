fb51f598a2a54c74be840836039a4b64f3e81c63
Jabran Rafique <jabran.rafique@ratedpeople.com>
2023-10-27 10:11:47 +0100

    update test setup

--- setup-test-app.sh
+++ setup-test-app.sh
@@ -1,60 +1,32 @@
 #!/usr/bin/env bash
 
 
+npx create-react-app test-app --template=typescript
 
-mkdir -p test-app/src test-app/public
+# mkdir -p test-app/src test-app/public
 
 npm pack
 mv use-gmaps*.tgz test-app/
 
 cd test-app
 
+# remove unused files
+rm src/App.css src/App.test.tsx src/logo.svg
+
+# install packaged version of use-gmaps
+npm i ./use-gmaps*.tgz
+rm ./use-gmaps*.tgz
+
+# setup app.tsx with use-gmaps
 echo "
-<!DOCTYPE html>
-<html lang=\"en\">
-  <head>
-    <meta charset=\"utf-8\" />
-    <meta
-      name=\"viewport\"
-      content=\"width=device-width, initial-scale=1, shrink-to-fit=no\"
-    />
-    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge,chrome=1\" />
-  </head>
-  <body>
-    <div id=\"root\"></div>
-  </body>
-</html>
-
-" > public/index.html
-
-
-echo "
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-
-import App from './app';
-
-const root = createRoot(document.querySelector('#root'));
-root.render(<App />);
-
-" > src/index.js
-
-echo "
-import React from 'react';
 import useGoogleMaps from 'use-gmaps';
 
 export default function App() {
   const { mapRef } = useGoogleMaps({ apiKey: 'ABC-XYZ' });
-
   return <div ref={mapRef} style={{ width: '100vw', height: '100vh' }} />;
 }
 
-" > src/app.js
-
-npm init -y
-npm i -D react react-dom react-scripts
-npm i ./use-gmaps*.tgz
-rm ./use-gmaps*.tgz
+" > src/App.tsx
 
 echo "**************************************";
 echo " ";
